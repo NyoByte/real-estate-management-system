45995bed1c47b02934912cad42974b52e9194c74
vigacv <vgabrielcv@gmail.com>
2021-10-04 22:12:16 -0500

    Sell and rent working

--- contracts/RealEstateManagement.sol
+++ contracts/RealEstateManagement.sol
@@ -21,15 +21,21 @@ contract RealEstateManagement{
         uint sellPercentage;
         uint sellPrice;
         bool exists;
+        bool completed;
     }
+
+    enum RentState{CREATED, STARTED, TERMINATED}
 
     struct Rent{
         uint id;
         bytes32 propertyHash;
-        bytes32 rentedFrom;
-        bytes32 rentedTo;
-        uint depositedValue;
-        uint rentValue;   
+        address[] rentedFrom;
+        address representative;
+        bool[] agreedOwners;
+        address rentedTo;
+        uint securityDeposit;
+        uint rentValue;
+        RentState state;
     }
 
     struct UserOwned{
@@ -105,7 +111,10 @@ contract RealEstateManagement{
     //event addNewDepartment(bytes32 propertyHash);
     //event addNewHouse(bytes32 propertyHash);
     event creationOfUser(address accountAddress);
-    event creationOfSell(bytes32 sellHash);
+    event creationOfSell(uint id, bytes32 sellHash);
+    event creationOfPaySell(uint id, bytes32 sellHash);
+    event creationOfRent(uint id, bytes32 rentHash);
+    event creationOfPayRent(uint id, bytes32 rentHash);
 
     constructor() public{
         contractOwner = msg.sender;
@@ -204,9 +213,23 @@ contract RealEstateManagement{
 
 
     //Funciones Sell
-    function createSell(uint _id, uint _price, bytes32 _propertyHash, address _sellTo, uint16 _sellPercentage) public{
-        require(userPropertyOwnedMapping[msg.sender].propertyHash == _propertyHash, "You are not owner of the property");
-        require(userPropertyOwnedMapping[msg.sender].percentOwn <= _sellPercentage, "You don't have enough property percentage");
+    function createSell(uint _price, bytes32 _propertyHash, address _sellTo, uint16 _sellPercentage) public{
+        require(userMapping[msg.sender].exists == true, "Only registered users can create sells");
+        require(userMapping[_sellTo].exists == true, "You can only sell to a registered user");
+
+        //Verificar que sea owner y que posea el porcentaje que se desea vender
+        address[] memory propOwners = propertyMapping[_propertyHash].owners;
+        uint[] memory propPercOwn = propertyMapping[_propertyHash].percentOwn;
+        bool isOwner;
+        for(uint i=0; i<propOwners.length; i++){
+            if(propOwners[i] == msg.sender){
+                require(propPercOwn[i] >= _sellPercentage, "You don't have enough property percentage");
+                isOwner=true;
+            }
+        }
+        require(isOwner, "You are not owner of the property");
+
+        uint _id = sellArray.length;
         bytes32 sellHash = keccak256(abi.encodePacked(_id, _propertyHash));
         sellMapping[sellHash].id = _id;
         sellMapping[sellHash].propertyHash = _propertyHash;
@@ -216,7 +239,9 @@ contract RealEstateManagement{
         sellMapping[sellHash].sellPercentage = _sellPercentage;
         sellMapping[sellHash].sellPrice = _price*10**18;
         sellMapping[sellHash].exists = true;
+        sellMapping[sellHash].completed = false;
         sellArray.push(sellHash);
+        emit creationOfSell(_id, sellHash);
     }
     
     function getSellHashById(uint _id) public view returns (bytes32 sellHash){
@@ -226,30 +251,117 @@ contract RealEstateManagement{
 
     function paySell(bytes32 _sellHash) public payable returns (bool message){
         require(msg.sender == sellMapping[_sellHash].sellTo, "You are not the buyer of this sell");
-        require(msg.value == sellMapping[_sellHash].sellPrice, string(abi.encodePacked("The amount is not correct, you sent ",msg.value," and required ",sellMapping[_sellHash].sellPrice)));
+        require(msg.value == sellMapping[_sellHash].sellPrice, "The amount is not correct");
         payable(sellMapping[_sellHash].buyFrom).transfer(msg.value);
 
-
         for(uint i=0; i<propertyMapping[sellMapping[_sellHash].propertyHash].owners.length; i++){
-            if(propertyMapping[sellMapping[_sellHash].propertyHash].owners[i] == sellMapping[_sellHash].buyFrom)
-                delete propertyMapping[sellMapping[_sellHash].propertyHash].owners[i];
-                delete propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn[i];
+            if(propertyMapping[sellMapping[_sellHash].propertyHash].owners[i] == sellMapping[_sellHash].buyFrom){
+                uint leftPercentage = propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn[i] - sellMapping[_sellHash].sellPercentage;
+                if(leftPercentage <= 0){
+                    propertyMapping[sellMapping[_sellHash].propertyHash].owners[i] = propertyMapping[sellMapping[_sellHash].propertyHash].owners[propertyMapping[sellMapping[_sellHash].propertyHash].owners.length-1];
+                    propertyMapping[sellMapping[_sellHash].propertyHash].owners.pop();
+
+                    propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn[i] = propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn[propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn.length -1];
+                    propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn.pop();
+                }else{
+                    propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn[i] = leftPercentage;
+                }
+            }
         }     
 
         propertyMapping[sellMapping[_sellHash].propertyHash].owners.push(msg.sender);
         propertyMapping[sellMapping[_sellHash].propertyHash].percentOwn.push(sellMapping[_sellHash].sellPercentage);
+        sellMapping[_sellHash].completed = true;
         return true;
     }
-    
+
+    //Funciones Rent
+    function createRent(bytes32 _propertyHash, address _rentedTo, uint _securityDeposit, uint _rentValue) public{
+        require(userMapping[msg.sender].exists == true, "Only registered users can create rents");
+        require(userMapping[_rentedTo].exists == true, "You can only rent to a registered user");
+
+        //Verificar que sea owner
+        address[] memory propOwners = propertyMapping[_propertyHash].owners;
+        bool isOwner;
+        uint ownerIndex;
+        for(uint i=0; i<propOwners.length; i++){
+            if(propOwners[i] == msg.sender){
+                isOwner=true;
+                ownerIndex = i;
+            }
+        }
+        require(isOwner, "You are not owner of the property");
+
+        uint _id = rentArray.length;
+        bytes32 rentHash = keccak256(abi.encodePacked(_id, _propertyHash));
+        rentMapping[rentHash].id = _id;
+        rentMapping[rentHash].propertyHash = _propertyHash;
+        rentMapping[rentHash].rentedFrom = propOwners;
+        rentMapping[rentHash].agreedOwners = new bool[](propOwners.length);
+        rentMapping[rentHash].agreedOwners[ownerIndex] = true;
+        rentMapping[rentHash].rentedTo = _rentedTo;
+        rentMapping[rentHash].securityDeposit = _securityDeposit*10**18;
+        rentMapping[rentHash].rentValue = _rentValue*10**18;
+        rentMapping[rentHash].representative = msg.sender;
+        rentMapping[rentHash].state = RentState.CREATED;
+        rentArray.push(rentHash);
+    }
+
+    function agreeRent(bytes32 _rentHash) public{
+        require(userMapping[msg.sender].exists == true, "Only registered users can create rents");
+        address[] memory rentPropOwners = rentMapping[_rentHash].rentedFrom;
+
+        bool isOwner;
+        for(uint i=0; i<rentPropOwners.length; i++){
+            if(rentPropOwners[i] == msg.sender){
+                isOwner=true;
+                rentMapping[_rentHash].agreedOwners[i] = true;
+            }
+        }
+        require(isOwner, "You are not owner of the property");
+    }
+
+    function paySecurityDeposit(bytes32 _rentHash) public payable{
+        require(msg.sender == rentMapping[_rentHash].rentedTo, "You are not tenant of this rent");
+        require(msg.value == rentMapping[_rentHash].securityDeposit, "Wrong amount");
+        for(uint i=0; i<rentMapping[_rentHash].agreedOwners.length; i++){
+            require(rentMapping[_rentHash].agreedOwners[i]==true, "Not all property owners have signed the contract");
+        }
+        payable(rentMapping[_rentHash].representative).transfer(msg.value);
+        rentMapping[_rentHash].state = RentState.STARTED;
+    }
+
+    function payRent(bytes32 _rentHash) public payable{
+        require(rentMapping[_rentHash].state == RentState.STARTED, "Rent state not started");
+        require(msg.sender == rentMapping[_rentHash].rentedTo, "You are not tenant of this rent");
+        require(msg.value == rentMapping[_rentHash].rentValue, "Wrong amount");
+        payable(rentMapping[_rentHash].representative).transfer(msg.value);
+    }
+
+    function terminateRent(bytes32 _rentHash) public payable{
+        require(msg.sender == rentMapping[_rentHash].representative, "You are not representative of this rent owners");
+        payable(rentMapping[_rentHash].rentedTo).transfer(msg.value);
+        rentMapping[_rentHash].state = RentState.TERMINATED;
+    }
+
+    function getRentHashById(uint _id) public view returns(bytes32 _rentHash){
+        return rentArray[_id];
+    }
 
     //Funciones Property
     function createnNewProperty(string memory _province, string memory _district, string memory _addres, uint _area, address[] memory _owners, uint16[] memory _percentOwn) public{
-        require(msg.sender == contractOwner, "You are not unauthorized");
+        require(msg.sender == contractOwner, "You are not authorized");
         _province = _toLower(_province);
         _district = _toLower(_district);
         _addres = _toLower(_addres);
         bytes32 propertyHash = keccak256(abi.encodePacked(_province, _district, _addres, _area));
         require(propertyMapping[propertyHash].exists == false, "Property already exists");
+        for(uint i=0; i<_owners.length;i++){
+            require(userMapping[_owners[i]].exists==true,"User does not exists");
+            userPropertyOwnedMapping[_owners[i]].propertyHash = propertyHash;
+            userPropertyOwnedMapping[_owners[i]].percentOwn = _percentOwn[i];
+        }
+        //require(userMapping[accountaddress].exists);
         propertyMapping[propertyHash].province = stringToBytes32(_province);
         propertyMapping[propertyHash].district = stringToBytes32(_district);
         propertyMapping[propertyHash].addres = stringToBytes32(_addres);
@@ -258,11 +370,12 @@ contract RealEstateManagement{
         propertyMapping[propertyHash].owners = _owners;
         propertyMapping[propertyHash].percentOwn = _percentOwn;
 
+        /*
         for(uint i=0; i<_owners.length; i++){
             userPropertyOwnedMapping[_owners[i]].propertyHash = propertyHash;
             userPropertyOwnedMapping[_owners[i]].percentOwn = _percentOwn[i];
         }
-
+        */
         propertyMapping[propertyHash].exists = true;
         propertyArray.push(propertyHash);
         emit addNewProperty(propertyHash);
