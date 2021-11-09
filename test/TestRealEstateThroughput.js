0fb8297a4e705372ed66f37c509917ef5d6f0b21
vigacv <vgabrielcv@gmail.com>
2021-11-08 20:16:15 -0500

    Agregadas nuevas pruebas

---
+++ test/TestRealEstateThroughput.js
@@ -0,0 +1,27 @@
+var RealEstateManagement = artifacts.require("./RealEstateManagement.sol");
+
+contract('RealEstateManagement', function (accounts) {
+    //let rem = RealEstateManagement.new( {from: accounts[0]});
+    before(async function () {
+        rem = await RealEstateManagement.new({ from: accounts[1] });
+        //Usuarios para Nyo-Poa
+        //admin
+        await rem.createNewUser("User1", "xxx", "Lima", "La Molina", accounts[1], "12345678", { from: accounts[1] });
+        await rem.createNewUser("User1", "xxx", "Lima", "La Molina", accounts[2], "12345678", { from: accounts[1] });
+
+        await rem.createnNewProperty("Lima", "San Luis", "Av. Siempre viva 123", 120, [accounts[1]], [100], { from: accounts[1] })
+
+        propertyHash = await rem.getPropertyHashById(0)
+
+    })
+
+    it("Test multiple sells 1", () => {
+        console.log("Starting...")
+        for(i=2;i<9;i++){
+            rem.createSell(2,propertyHash,accounts[i],1, {from: accounts[1]}).then(function (tx) {
+                console.log("Sell created")
+                console.log(tx)
+            })
+        }
+    });
+});
